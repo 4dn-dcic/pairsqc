52a5f970b808a437ef895354db277b8f1a339e48
SooLee <duplexa@gmail.com>
2017-02-09 17:59:45 -0500

    refined plots

--- plot.r
+++ plot.r
@@ -5,11 +5,16 @@ x=read.table("report/plot_table.out",skip=0,sep="\t",stringsAsFactors=F,header=T
 
 dir.create("report/plots", showWarnings = FALSE, recursive = TRUE)
 setwd("report/plots")
-pngpdf.nodate( function()plot_orientation_proportion_vs_distance(x), "proportion" )
+pngpdf.nodate( function(){
+  plot_orientation_proportion_vs_distance(x, plt=c(0.2,0.8,0.45,0.9), no_xlabel=T)
+  par(new=T)
+  plot_sd_with_cutoff(x, plt=c(0.2,0.8,0.2,0.35))
+}, "proportion" )
+
 pngpdf.nodate( function()plot_orientation_log10count_vs_distance(x) , "log10counts" )
 pngpdf.nodate( function()plot_contact_probability_vs_distance(x) ,"log10prob")
 pngpdf.nodate( function()plot_entropy(x), "entropy", height=4)
-pngpdf.nodate( function()plot_sd_with_cutoff(x),"sd_w_cut", height=4)
+#pngpdf.nodate( function()plot_sd_with_cutoff(x),"sd_w_cut", height=4)
 
 setwd("..")
 generate_pairsqc_report()
--- plot_functions.r
+++ plot_functions.r
@@ -10,35 +10,38 @@ exp_axis<-function(x, axis_ind){
 
 pngpdf.nodate <- function(f,...) pngpdf(f,...,add.date=FALSE)
 
-plot_orientation_proportion_vs_distance <- function(x, xlim=c(2,4), plt=c(0.2,0.8,0.2,0.8)){
+plot_orientation_proportion_vs_distance <- function(x, xlim=c(2,4), plt=c(0.2,0.8,0.2,0.8), no_xlabel=FALSE){
   par(plt=plt)
-  matplot(x$distance,x[,12:15],pch=19,type='o',xlab="distance (log10)", ylab="Proportion",lwd=1,lty=1, axes=F, xlim=xlim)
-  exp_axis(x$distance,1)
+  matplot(x$distance,x[,12:15],pch=19,type='o',xlab="", ylab="Proportion",lwd=1,lty=1, axes=F, xlim=xlim)
+  exp_axis(xlim,1)
   axis(2)
   box()
+  if(no_xlabel==FALSE) mtext("distance",side=1,line=2.5)
   legend("topright",sub('proportion.','',colnames(x)[12:15]),col=1:4,bty="n",pch=19,lwd=1,lty=1)
 } 
 
 
-plot_orientation_log10count_vs_distance <- function(x, xlim=c(2,4), plt=c(0.2,0.8,0.2,0.8)){
+plot_orientation_log10count_vs_distance <- function(x, xlim=c(2,4), plt=c(0.2,0.8,0.2,0.8), no_xlabel=FALSE){
   par(plt=plt)
   ylim=c(0,max(x[,7:10]))
-  matplot(x$distance,x[,7:10],pch=19,type='o',xlab="distance (log10)", ylab="Contact frequency (log10)",lwd=1,lty=1,ylim=ylim, axes=F, xlim=xlim)
+  matplot(x$distance,x[,7:10],pch=19,type='o',xlab="", ylab="Contact frequency",lwd=1,lty=1,ylim=ylim, axes=F, xlim=xlim)
   exp_axis(xlim,1)
   exp_axis(ylim,2)
   box()
+  if(no_xlabel==FALSE) mtext("distance",side=1,line=2.5)
   legend("bottomright",sub('log10count.','',colnames(x)[7:10]),col=1:4,bty="n",pch=19,lwd=1,lty=1)
 }
 
 
-plot_contact_probability_vs_distance <- function(x, xlim=c(3.2,8), plt=c(0.2,0.8,0.2,0.8)) {
+plot_contact_probability_vs_distance <- function(x, xlim=c(3.2,8), plt=c(0.2,0.8,0.2,0.8), no_xlabel=FALSE) {
   par(plt=plt)
   ylim=range(x[,18])
-  plot(x$distance,x[,18],pch=19,type='o',xlab="distance (log10)", ylab="Contact probability (log10)",lwd=1,lty=1,ylim=ylim,axes=F,xlim=xlim)
+  plot(x$distance,x[,18],pch=19,type='o',xlab="", ylab="Contact probability",lwd=1,lty=1,ylim=ylim,axes=F,xlim=xlim)
   exp_axis(xlim,1)
   exp_axis(ylim,2)
   box()
-  
+  if(no_xlabel==FALSE) mtext("distance",side=1,line=2.5)  
+
   # slope
   tad=which(x$distance>=4 & x$distance<=5.5)
   xx=x$distance[tad]
@@ -56,14 +59,19 @@ entropy<-function(xx)-sum(xx*log2(xx))
 plot_entropy <- function(x, xlim=c(2,4), plt=c(0.2,0.8,0.2,0.8)){
   par(plt=plt)
   entropies=apply(x[,12:15],1,entropy)
-  plot(entropies,type='o',pch=19,ylab="entropy",xlab="distance",xlim=xlim)
+  plot(entropies,type='o',pch=19,ylab="entropy",xlab="distance",xlim=xlim,axes=F)
+  exp_axis(x$distance,1)
+  axis(2)
 }
 
 
 plot_sd_with_cutoff <- function(x, xlim=c(2,4), plt=c(0.2,0.8,0.2,0.8)){
   par(plt=plt)
   sds=apply(x[,12:15],1,sd)
-  plot(x$distance, sds,type='o',pch=19,ylab="sd",xlab="distance",xlim=xlim)
+  plot(x$distance, sds,type='o',pch=19,ylab="sd",xlab="distance",xlim=xlim,axes=F)
+  exp_axis(x$distance,1)
+  axis(2)
+  box()
   cut = min(which(sds<0.02))
   abline(v=x[cut ,"distance"],col=2,lty=2)
 }
