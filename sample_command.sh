343364b0bb423598a184482c363393fdf50f2013
soolee <duplexa@gmail.com>
2017-02-10 22:13:30 -0500

    updated sample_command.sh

--- sample_command.sh
+++ sample_command.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python pairsqc.py --pairs /n/data1/hms/dbmi/park/sl325/4dn/pairs/K562_insitu_merged_nodups.tab.chrblock_sorted.-4.txt.gz --chrsize /n/data1/hms/dbmi/park/sl325/4dn/pairs/hg19.chrom.sizes.mainonly 
+python pairsqc.py --pairs /n/data1/hms/dbmi/park/sl325/4dn/pairs/K562_insitu_merged_nodups.tab.chrblock_sorted.-4.txt.gz --chrsize /n/data1/hms/dbmi/park/sl325/4dn/pairs/hg19.chrom.sizes.mainonly -t OM
