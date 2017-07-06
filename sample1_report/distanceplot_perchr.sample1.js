72133a7893a8151b6831304536409978d81cfcf3
SooLee <duplexa@gmail.com>
2017-07-06 16:44:33 -0400

    put report directory back

---
+++ sample1_report/distanceplot_perchr.sample1.js
@@ -0,0 +1,3 @@
+d3.tsv('./tsvcol.d3div_contact_frequency_vs_genomic_separation_per_chr_sample1___.tsv', function(tsvcolumns) {
+interactive_multiline_plot('./sample1.plot_table.out', tsvcolumns, 3.5,7,-12.307,-8.371, 'log10 genomic separation', 'log10 contact frequency', 'd3div_contact_frequency_vs_genomic_separation_per_chr_sample1___');
+});
